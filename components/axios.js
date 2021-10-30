85912c663a4bce54c867910bb6774775e4a30196
jamesyeogz <84173775+jamesyeogz@users.noreply.github.com>
2021-10-30 18:44:36 +0800

    Update axios.js

--- components/axios.js
+++ components/axios.js
@@ -4,10 +4,10 @@ const baseURL ="https://polar-citadel-27951.herokuapp.com/api"
 
 const api = axios.create({
   baseURL: baseURL,
-//   headers: {
-//     "X-Requested-With": "XMLHttpRequest",
-//     "Access-Control-Allow-Origin": "*",
-//   },
+     headers: {
+     "X-Requested-With": "XMLHttpRequest",
+     "Access-Control-Allow-Origin": "*",
+   },
 });
 
 export default api;
