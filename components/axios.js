87323a95343fd05650c459c74a38b20e790f179a
jamesyeogz <84173775+jamesyeogz@users.noreply.github.com>
2021-10-30 18:24:51 +0800

    Update axios.js

--- components/axios.js
+++ components/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 // const baseURL = "http://127.0.0.1:8000/api";
-const baseURL ="http://polar-citadel-27951.herokuapp.com/api"
+const baseURL ="https://polar-citadel-27951.herokuapp.com/api"
 
 const api = axios.create({
   baseURL: baseURL,
