f0619b98e785d4125a223e08fdcc87c9f6cf5a8c
jamesyeogz <84173775+jamesyeogz@users.noreply.github.com>
2021-10-30 18:19:25 +0800

    Update axios.js

--- components/axios.js
+++ components/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
-const baseURL = "http://127.0.0.1:8000/api";
+// const baseURL = "http://127.0.0.1:8000/api";
+const baseURL ="http://polar-citadel-27951.herokuapp.com/api"
 
 const api = axios.create({
   baseURL: baseURL,
-  // baseURL: "http://polar-citadel-27951.herokuapp.com/api",
 //   headers: {
 //     "X-Requested-With": "XMLHttpRequest",
 //     "Access-Control-Allow-Origin": "*",
